e47723d0f5c1eca7b1239c61d2784aeb0ef37ae2
BrysonHinsch <brysonhinsch@gmail.com>
2026-04-18 21:51:23 -0400

    Implemented Code Input Box

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -3,6 +3,13 @@ const canvas = document.getElementById('mainCanvas');
 const ctx = canvas.getContext('2d');
 const state = {};
 
+let record = false; // When true begins recording gif
+let frames = 0;     // Number of frames read so far
+
+var encoder = new GIFEncoder();
+encoder.setRepeat(0);
+encoder.setDelay(1000/60);
+
 // Determines the functionality of the canvas
 // Whatever the user types into the text box will replace this function
 
@@ -12,6 +19,19 @@ let canvasFunction = function(canvas, ctx, state) {}
 
 function canvasLoop() {
     canvasFunction(canvas, ctx, state);
+    if (record == true) {
+        frames++
+        if (frames % 4 == 0) {
+            encoder.addFrame(ctx);
+        }
+        console.log(frames);
+        if (frames == 1000) {
+            encoder.finish();
+            encoder.download("download.gif");
+            record = false;
+            frames = 0;
+        }
+    }
     requestAnimationFrame(canvasLoop);
 }
 
@@ -30,15 +50,15 @@ function resetCanvas() {
 
 function saveCanvas() {
     // Will eventually save the canvas as a gif
-
-    console.log("Canvas Saved");
+    record = true;
+    encoder.start();
 }
 
 // Updates the functionality of the canvas function to what the user has typed in
 
 function updateCanvasFunction() {
     resetCanvas();
-    canvasFunction = new Function(document.getElementById("inputbox").value);
+    canvasFunction = new Function("canvas", "ctx", "state", document.getElementById("inputbox").value);
     console.log("Canvas Updated");
 }
 
